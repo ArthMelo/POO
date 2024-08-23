bdca77e0e1bc138be5974aeae747cd62dba47444
Arthur Melo <arthurmb@dcc.ufrj.br>
2024-08-23 15:46:16 -0300

    terminando lab 2 - lista 2

--- Lab2.java
+++ Lab2.java
@@ -1,58 +1,14 @@
-package principal;
-import java.util.*;
+public class Lab2 {
+    public static void main(String[] args){
+        Funcionario funcionario1 = new Funcionario("Luiz", 15.0);
+        funcionario1.adicionaDiaDeTrabalho(5);
+        funcionario1.atualizaNome("Luiz Henrique");
 
-public class Funcionario {
-	private String nome;
-	private double salarioHora;
-	private final int ID_FUNC;
-	public static final double SALARIO_HORA_MINIMO = 12.50;
-	private double horasTrabalhadasMes;
-	
-	public Funcionario(String name) {
-		this.nome = name;
-		this.ID_FUNC = getProximoID();
-	}
-	
-	public Funcionario(String name, double _salarioHora) {
-		this.nome = name;
-		this.salarioHora = _salarioHora;
-		this.ID_FUNC = getProximoID();
-	}
-	
-	public String getNome(){
-		return this.nome;
-	}
-	
-	public int getID() {
-		return this.ID_FUNC;
-	}
-	
-	public double getSalarioHora() {
-		return this.salarioHora;
-	}
-	
-	public double calculaSalarioMes() {
-		return this.horasTrabalhadasMes * this.salarioHora;
-	}
-	
-	public void reiniciaMesTrabalho() {
-		
-	}
-	
-	public void atualizaNome(String nome) {
-		
-	}
-	
-	public void atualizaSalarioHora(double salario) {
-		
-	}
-	
-	public int getProximoID() {
-		return this.ID_FUNC;
-	}
-	
-	public void adicionaDiaDeTrabalho(double horasTrabalhadas) {
-		
-	}
-
+        System.out.println("Nome: "+funcionario1.getNome());
+        System.out.println("ID: "+funcionario1.getID());
+        System.out.println("Salario por Hora: "+funcionario1.getSalarioHora());
+        System.out.println("Salario: "+funcionario1.calculaSalarioMes());
+        funcionario1.reiniciaMesTrabalho();
+        
+    }
 }
